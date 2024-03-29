8252167eea5a3819286d8dc11eabf5958160cff4
dario248 <paezdario24@gmail.com>
2024-03-29 10:15:53 -0300

    fix: aws credentials

--- getusers/handler.js
+++ getusers/handler.js
@@ -1,14 +1,19 @@
 const aws = require("aws-sdk")
 
+// Configure AWS profile crendentials
+const profile_name = 'free-user'; 
+const credentials = new aws.SharedIniFileCredentials({ profile: profile_name });
+aws.config.credentials = credentials;
+
 let dynamoDBClientParams = {}
 
 if (process.env.IS_OFFLINE) {
     dynamoDBClientParams =  {
         region: 'localhost',
         endpoint: 'http://localhost:8000',
-        accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
-        secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
-    }
+        };
+} else {
+    dynamoDBClientParams = {};
 }
 
 const dynamodb = new aws.DynamoDB.DocumentClient(dynamoDBClientParams)
